ef7e74c52be3c1fe1a6eaaa09b55bc4735d4aeb5
Yash Dongardiye <115202682+yashDongardiye@users.noreply.github.com>
2025-07-18 18:27:57 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -2,10 +2,10 @@ variable "region" {
     default = "ap-south-1" 
 }
 variable "access_key" {
-    default = "AKIAW5BDQWYG4T3AAVJ6"
+    default = "AKIA************AVJ6"
     sensitive = true
 }
 variable "secret_key" {
-    default = "ISXGgSuJEbFaGt51dNEXisl2XiqJttQkQgri5ARb"
+    default = "ISX*******************************ri5ARb"
     sensitive = true
 }
